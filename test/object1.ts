99d92fd4d7b4c1129abd49c233d48a4e896287b6
Michel Weststrate <mweststrate@gmail.com>
2016-12-30 15:55:50 +0100

    Fix test

--- test/object1.ts
+++ test/object1.ts
@@ -1,6 +1,5 @@
 import {onSnapshot, onPatch, createFactory, applyPatch, _getNode, getPath, IJsonPatch, applySnapshot} from "../"
 import {test} from "ava"
-import "jest"
 
 test("it should create a factory", (t) => {
     const factory = createFactory({
