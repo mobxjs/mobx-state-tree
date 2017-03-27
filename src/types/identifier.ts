2d88f1ab869f69d5f0f37748210e76f833812e74
Michel Weststrate <mweststrate@gmail.com>
2017-03-27 22:11:40 +0200

    Added small todo

--- src/types/identifier.ts
+++ src/types/identifier.ts
@@ -2,6 +2,7 @@ export interface IIdentifierDescriptor {
     isIdentifier: true
 }
 
+// TODO: properly turn this into a factory, that reuses `types.string`?
 export function identifier(): string {
     return {
         isIdentifier: true
