f05e88a1009d8c251f7e02654aba1139472777fb
mondaychen <monday.chen@gmail.com>
2017-06-19 17:15:00 -0400

    Add todomvc example

---
+++ examples/todomvc/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+import 'todomvc-app-css/index.css'
+import { connectReduxDevtools } from 'mobx-state-tree'
+
+import TodoStore from './models/todos'
+
+const store = TodoStore.create({
+  todos: [
+    {
+      text: 'learn Mobx',
+      completed: false,
+      id: 0
+    },
+    {
+      text: 'learn MST',
+      completed: false,
+      id: 1
+    }
+  ]
+})
+connectReduxDevtools(require('remotedev'), store)
+
+render(<App store={store} />, document.getElementById('root'))
---
+++ examples/todomvc/src/models/todos.js
@@ -0,0 +1,48 @@
+import { types, destroy } from 'mobx-state-tree'
+
+const Todo = types.model(
+  {
+    text: 'Learn Redux',
+    completed: false,
+    id: 0
+  },
+  {
+    remove() {
+      destroy(this)
+    },
+
+    edit(text) {
+      this.text = text
+    },
+
+    complete() {
+      this.completed = !this.completed
+    }
+  }
+)
+
+const TodoStore = types.model(
+  {
+    todos: types.array(Todo)
+  },
+  {
+    // actions
+    addTodo(text) {
+      const id =
+        this.todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1
+      this.todos.unshift({
+        id,
+        text
+      })
+    },
+    completeAll() {
+      const areAllMarked = this.todos.every(todo => todo.completed)
+      this.todos.forEach(todo => (todo.completed = !areAllMarked))
+    },
+    clearCompleted() {
+      this.todos.replace(this.todos.filter(todo => todo.completed === false))
+    }
+  }
+)
+
+export default TodoStore
