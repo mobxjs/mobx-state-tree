c622e7e09836f37e38bbebc3e7294a338c3af2d2
Michel Weststrate <mweststrate@gmail.com>
2017-11-19 20:34:50 +0100

    Added custom references

--- packages/mobx-state-tree/src/types/utility-types/reference.ts
+++ packages/mobx-state-tree/src/types/utility-types/reference.ts
@@ -1,17 +1,12 @@
-import { getStateTreeNode, isStateTreeNode, Node, createNode } from "../../core"
+import { createNode, getStateTreeNode, isStateTreeNode, IStateTreeNode, Node } from "../../core"
 import { Type, IType } from "../type"
 import { TypeFlags, isReferenceType, isType } from "../type-flags"
-import {
-    IContext,
-    IValidationResult,
-    typeCheckSuccess,
-    typeCheckFailure,
-    prettyPrintValue
-} from "../type-checker"
+import { IContext, IValidationResult, typeCheckSuccess, typeCheckFailure } from "../type-checker"
 import { fail } from "../../utils"
+import { action } from "mobx"
 
 class StoredReference {
-    constructor(public mode: "identifier" | "object", public value: any) {
+    constructor(public readonly mode: "identifier" | "object", public readonly value: any) {
         if (mode === "object") {
             if (!isStateTreeNode(value))
                 return fail(`Can only store references to tree nodes, got: '${value}'`)
@@ -25,8 +20,15 @@ class StoredReference {
 export class ReferenceType<T> extends Type<string | number, T> {
     readonly flags = TypeFlags.Reference
 
-    constructor(private readonly targetType: IType<any, T>) {
+    constructor(
+        private readonly targetType: IType<any, T>,
+        private readonly options?: ReferenceOptions<T>
+    ) {
         super(`reference(${targetType.name})`)
+    }
+
+    get isCustomReference() {
+        return !!this.options
     }
 
     describe() {
@@ -34,18 +36,24 @@ export class ReferenceType<T> extends Type<string | number, T> {
     }
 
     getValue(node: Node) {
-        const ref = node.storedValue as StoredReference
-        if (ref.mode === "object") return ref.value
-
         if (!node.isAlive) return undefined
-        // reference was initialized with the identifier of the target
-        const target = node.root.identifierCache!.resolve(this.targetType, ref.value)
-        if (!target)
-            return fail(
-                `Failed to resolve reference of type ${this.targetType
-                    .name}: '${ref.value}' (in: ${node.path})`
-            )
-        return target.value
+        const ref = node.storedValue as StoredReference
+
+        if (!this.isCustomReference) {
+            // id already resolved, return
+            if (ref.mode === "object") return ref.value
+
+            // reference was initialized with the identifier of the target
+            const target = node.root.identifierCache!.resolve(this.targetType, ref.value)
+            if (!target)
+                return fail(
+                    `Failed to resolve reference of type ${this.targetType
+                        .name}: '${ref.value}' (in: ${node.path})`
+                )
+            return target.value
+        } else {
+            return this.options!.get(ref.value, node.parent ? node.parent.storedValue : null)
+        }
     }
 
     getSnapshot(node: Node): any {
@@ -60,15 +68,30 @@ export class ReferenceType<T> extends Type<string | number, T> {
 
     instantiate(parent: Node | null, subpath: string, environment: any, snapshot: any): Node {
         const isComplex = isStateTreeNode(snapshot)
-        return createNode(
-            this,
-            parent,
-            subpath,
-            environment,
-            new StoredReference(isComplex ? "object" : "identifier", snapshot)
-        )
+        if (!this.isCustomReference)
+            return createNode(
+                this,
+                parent,
+                subpath,
+                environment,
+                new StoredReference(isComplex ? "object" : "identifier", snapshot)
+            )
+        else
+            return createNode(
+                this,
+                parent,
+                subpath,
+                environment,
+                new StoredReference(
+                    "identifier",
+                    isComplex
+                        ? this.options!.set(snapshot, parent ? parent.storedValue : null)
+                        : snapshot
+                )
+            )
     }
 
+    @action
     reconcile(current: Node, newValue: any): Node {
         const targetMode = isStateTreeNode(newValue) ? "object" : "identifier"
         if (isReferenceType(current.type)) {
@@ -100,7 +123,15 @@ export class ReferenceType<T> extends Type<string | number, T> {
     }
 }
 
-export function reference<T>(factory: IType<any, T>): IType<string | number, T>
+export type ReferenceOptions<T> = {
+    get(identifier: string | number, parent: IStateTreeNode | null): T
+    set(value: T, parent: IStateTreeNode | null): string | number
+}
+
+export function reference<T>(
+    factory: IType<any, T>,
+    options?: ReferenceOptions<T>
+): IType<string | number, T>
 /**
  * Creates a reference to another type, which should have defined an identifier.
  * See also the [reference and identifiers](https://github.com/mobxjs/mobx-state-tree#references-and-identifiers) section.
@@ -108,7 +139,7 @@ export function reference<T>(factory: IType<any, T>): IType<string | number, T>
  * @export
  * @alias types.reference
  */
-export function reference<T>(subType: IType<any, T>): any {
+export function reference<T>(subType: IType<any, T>, options?: ReferenceOptions<T>): any {
     // check that a type is given
     if (process.env.NODE_ENV !== "production") {
         if (!isType(subType))
@@ -116,5 +147,5 @@ export function reference<T>(subType: IType<any, T>): any {
         if (arguments.length === 2 && typeof arguments[1] === "string")
             fail("References with base path are no longer supported. Please remove the base path.")
     }
-    return new ReferenceType(subType)
+    return new ReferenceType(subType, options)
 }
---
+++ packages/mobx-state-tree/test/reference-custom.ts
@@ -0,0 +1,87 @@
+import { reaction } from "mobx"
+import {
+    types,
+    recordPatches,
+    getSnapshot,
+    applySnapshot,
+    applyPatch,
+    unprotect,
+    getRoot,
+    onSnapshot
+} from "../src"
+import { test } from "ava"
+
+test("it should support custom references", t => {
+    const User = types.model({
+        id: types.identifier(),
+        name: types.string
+    })
+    type IUser = typeof User.Type
+
+    const NameReference = types.reference(User, {
+        get(identifier: string, parent) {
+            if (identifier === null) return null
+            return (
+                getRoot(parent!)
+                    .users.values()
+                    .filter(u => u.name === identifier)[0] || null
+            )
+        },
+        set(value: IUser): string {
+            return value ? value.name : ""
+        }
+    })
+
+    const Store = types.model({
+        users: types.map(User),
+        selection: NameReference
+    })
+
+    const s = Store.create({
+        users: {
+            "1": { id: "1", name: "Michel" },
+            "2": { id: "2", name: "Mattia" }
+        },
+        selection: "Mattia"
+    })
+    unprotect(s)
+
+    t.is(s.selection.name, "Mattia")
+    t.true(s.selection === s.users.get("2"))
+    t.is((getSnapshot(s) as any).selection, "Mattia")
+
+    const p = recordPatches(s)
+    const r: any[] = []
+    onSnapshot(s, r.push.bind(r))
+    const ids: (string | null)[] = []
+
+    reaction(
+        () => s.selection,
+        selection => {
+            ids.push(selection ? selection.id : null)
+        }
+    )
+
+    s.selection = s.users.get("1")
+
+    t.is(s.selection.name, "Michel")
+    t.true(s.selection === s.users.get("1"))
+    t.is((getSnapshot(s) as any).selection, "Michel")
+
+    applySnapshot(s, { ...getSnapshot(s), selection: "Mattia" })
+    t.is(s.selection, s.users.get("2"))
+
+    applyPatch(s, { op: "replace", path: "/selection", value: "Michel" })
+    t.is(s.selection, s.users.get("1"))
+
+    s.users.delete("1")
+    t.is(s.selection, null)
+
+    s.users.put({ id: "3", name: "Michel" })
+    t.is(s.selection.id, "3")
+
+    t.snapshot(ids) // N.B. the first `null` is a bug in mobx, but only pops up when not using a proper action wrapper
+    t.snapshot(r)
+    t.snapshot(p.patches)
+    t.snapshot(p.inversePatches)
+})
