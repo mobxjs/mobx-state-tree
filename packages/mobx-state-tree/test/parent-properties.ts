b5ebbbdc1b5340c197fd6c7dbe956ce2f65e61e5
Michel Weststrate <mweststrate@gmail.com>
2017-11-28 15:17:45 +0100

    Improved test

--- packages/mobx-state-tree/test/parent-properties.ts
+++ packages/mobx-state-tree/test/parent-properties.ts
@@ -4,6 +4,7 @@ import { test } from "ava"
 const ChildModel = types
     .model("Child", {
         parentPropertyIsNullAfterCreate: false,
+        parentEnvIsNullAfterCreate: false,
         parentPropertyIsNullAfterAttach: false
     })
     .views(self => ({
@@ -14,27 +15,25 @@ const ChildModel = types
     .actions(self => ({
         afterCreate() {
             self.parentPropertyIsNullAfterCreate = typeof self.parent.fetch === "undefined"
+            self.parentEnvIsNullAfterCreate = typeof getEnv(self.parent).fetch === "undefined"
         },
         afterAttach() {
             self.parentPropertyIsNullAfterAttach = typeof self.parent.fetch === "undefined"
         }
     }))
 
-const ParentModel = types
-    .model("Parent", {
-        child: types.optional(ChildModel, {})
-    })
-    .views(self => ({
-        get fetch() {
-            return getEnv(self).fetch
-        }
-    }))
+const ParentModel = types.model("Parent", {}).props({
+    child: types.optional(ChildModel, {})
+})
 
-test("Parent property has value during child's afterCreate() event", t => {
+test("Parent property does not have value during child's afterCreate() event", t => {
     const mockFetcher = () => Promise.resolve(true)
     const parent = ParentModel.create({}, { fetch: mockFetcher })
 
-    t.is(parent.child.parentPropertyIsNullAfterCreate, false)
+    // Because the child is created before the parent creation is finished, this one will yield `true` (the .fetch view is still undefined)
+    t.is(parent.child.parentPropertyIsNullAfterCreate, true)
+    // ... but, the env is available
+    t.is(parent.child.parentEnvIsNullAfterCreate, false)
 })
 
 test("Parent property has value during child's afterAttach() event", t => {
