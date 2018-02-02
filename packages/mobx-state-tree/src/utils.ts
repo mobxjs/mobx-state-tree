49f5915a9f9af5ec023eadac1d11f161a541ebd2
Juan Patten <jr@juanpatten.com>
2018-02-02 12:20:09 -0700

    fix to typedefs for Typescript 2.7

--- packages/mobx-state-tree/src/utils.ts
+++ packages/mobx-state-tree/src/utils.ts
@@ -51,8 +51,8 @@ export function extendKeepGetter(a: any, ...b: any[]) {
     for (let i = 0; i < b.length; i++) {
         const current = b[i]
         for (let key in current) {
-            const descriptor = Object.getOwnPropertyDescriptor(current, key)
-            if ("get" in descriptor) {
+            const descriptor = Object.getOwnPropertyDescriptor(current, key) || {}
+            if (descriptor && "get" in descriptor) {
                 Object.defineProperty(a, key, { ...descriptor, configurable: true })
                 continue
             }
