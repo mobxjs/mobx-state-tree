d1844e4aca77adf8fab964835711bb8f9161debe
Michel Weststrate <mweststrate@gmail.com>
2018-06-28 15:14:15 +0200

    Made typecheck errors more readable

--- packages/mobx-state-tree/src/core/type/type-checker.ts
+++ packages/mobx-state-tree/src/core/type/type-checker.ts
@@ -70,7 +70,7 @@ function toErrorString(error: IValidationError): string {
         }` +
         (error.message ? ` (${error.message})` : "") +
         (type
-            ? isPrimitiveType(type) // TODO: or is primitive (value)
+            ? isPrimitiveType(type) || isPrimitive(value)
                 ? `.`
                 : `, expected an instance of \`${
                       type.name
