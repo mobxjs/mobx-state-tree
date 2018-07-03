2c4c5b44ec81aea8dd1799a35775bba9c577d1b7
Michel Weststrate <mweststrate@gmail.com>
2018-07-03 13:39:26 +0200

    Added test showing that #885 works

--- packages/mobx-state-tree/test/type-system.ts
+++ packages/mobx-state-tree/test/type-system.ts
@@ -1,4 +1,4 @@
-import { types, getSnapshot, unprotect } from "../src"
+import { types, getSnapshot, unprotect, IType } from "../src"
 const createTestFactories = () => {
     const Box = types.model({
         width: 0,
@@ -359,7 +359,10 @@ test("it should type compose correctly", () => {
                 log
             }
         })
-    const LoggableCar = types.compose(Car, Logger)
+    const LoggableCar = types.compose(
+        Car,
+        Logger
+    )
     const x = LoggableCar.create({ wheels: 3, logNode: "test" /* compile error: x: 7  */ })
     //x.test() // compile error
     x.drive()
@@ -390,7 +393,11 @@ test("it should extend types correctly", () => {
                 }
             }
         })
-    const LoggableCar = types.compose("LoggableCar", Car, Logger)
+    const LoggableCar = types.compose(
+        "LoggableCar",
+        Car,
+        Logger
+    )
     const x = LoggableCar.create({ wheels: 3, logNode: "test" /* compile error: x: 7  */ })
     // x.test() // compile error
     x.drive()
@@ -409,4 +416,29 @@ test("self referring views", () => {
         return views
     })
     expect(Car.create().tripple).toBe(9)
+})
+
+test("Alternative typeof syntax #885", () => {
+    const Car = types
+        .model({
+            wheels: 3
+        })
+        .actions(self => {
+            function drive() {}
+            return {
+                drive
+            }
+        })
+
+    type TypeOf<T extends IType<any, any, any>> = T extends IType<any, any, infer T2> ? T2 : never
+    type SnapshotTypeOf<T extends IType<any, any, any>> = T extends IType<any, infer S, any>
+        ? S
+        : never
+    type CreationTypeOf<T extends IType<any, any, any>> = T extends IType<infer C, any, any>
+        ? C
+        : never
+
+    type CarT = TypeOf<typeof Car>
+    type Car2 = typeof Car.Type
+    type CarSnapshot = SnapshotTypeOf<typeof Car>
 })
