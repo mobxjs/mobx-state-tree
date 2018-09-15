3526bf20abcda9ffeb1ca55a253b91b75e77e8ab
Javier Gonzalez <xaviergonz@gmail.com>
2018-09-15 16:26:20 +0200

    better flow typing for ts3

--- packages/mobx-state-tree/src/core/flow.ts
+++ packages/mobx-state-tree/src/core/flow.ts
@@ -1,37 +1,14 @@
-// based on: https://github.com/mobxjs/mobx-utils/blob/master/src/async-action.ts
+export interface FlowStep {
+    // fake, only for typing
+    "!!flowStep": undefined
+}
 
-export function flow<R>(generator: () => IterableIterator<any>): () => Promise<R>
-export function flow<A1>(generator: (a1: A1) => IterableIterator<any>): (a1: A1) => Promise<any> // Ideally we want to have R instead of Any, but cannot specify R without specifying A1 etc... 'any' as result is better then not specifying request args
-export function flow<A1, A2>(
-    generator: (a1: A1, a2: A2) => IterableIterator<any>
-): (a1: A1, a2: A2) => Promise<any>
-export function flow<A1, A2, A3>(
-    generator: (a1: A1, a2: A2, a3: A3) => IterableIterator<any>
-): (a1: A1, a2: A2, a3: A3) => Promise<any>
-export function flow<A1, A2, A3, A4>(
-    generator: (a1: A1, a2: A2, a3: A3, a4: A4) => IterableIterator<any>
-): (a1: A1, a2: A2, a3: A3, a4: A4) => Promise<any>
-export function flow<A1, A2, A3, A4, A5>(
-    generator: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => IterableIterator<any>
-): (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => Promise<any>
-export function flow<A1, A2, A3, A4, A5, A6>(
-    generator: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => IterableIterator<any>
-): (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => Promise<any>
-export function flow<A1, A2, A3, A4, A5, A6, A7>(
-    generator: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => IterableIterator<any>
-): (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => Promise<any>
-export function flow<A1, A2, A3, A4, A5, A6, A7, A8>(
-    generator: (
-        a1: A1,
-        a2: A2,
-        a3: A3,
-        a4: A4,
-        a5: A5,
-        a6: A6,
-        a7: A7,
-        a8: A8
-    ) => IterableIterator<any>
-): (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8) => Promise<any>
+// we skip promises that are the result of yielding promises
+export type FlowReturnType<R> = FlowStepOnlyToVoid<R extends Promise<any> ? FlowStep : R>
+
+// we extract yielded promises from the return type
+export type FlowStepOnlyToVoid<R> = Exclude<R, FlowStep> extends never ? void : Exclude<R, FlowStep>
+
 /**
  * See [asynchronous actions](https://github.com/mobxjs/mobx-state-tree/blob/master/docs/async-actions.md).
  *
@@ -39,8 +16,10 @@ export function flow<A1, A2, A3, A4, A5, A6, A7, A8>(
  * @alias flow
  * @returns {Promise}
  */
-export function flow(asyncAction: any): any {
-    return createFlowSpawner(asyncAction.name, asyncAction)
+export function flow<R, Args extends any[]>(
+    generator: (...args: Args) => IterableIterator<R>
+): (...args: Args) => Promise<FlowReturnType<R>> {
+    return createFlowSpawner(generator.name, generator) as any
 }
 
 /**
