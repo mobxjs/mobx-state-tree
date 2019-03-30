28459eceab6e5488d9c3065983fd70a883da85c1
Javier Gonzalez <xaviergonz@users.noreply.github.com>
2019-03-30 17:22:50 +0100

    upgrade dev deps (#1239)

--- packages/mobx-state-tree/src/types/complex-types/model.ts
+++ packages/mobx-state-tree/src/types/complex-types/model.ts
@@ -89,7 +89,7 @@ export type ModelPropertiesDeclarationToProperties<T extends ModelPropertiesDecl
         ? IType<boolean | undefined, boolean, boolean>
         : T[K] extends Date
         ? IType<number | Date | undefined, number, Date>
-        : T[K] extends IType<infer _TC, infer _TS, infer _TT> // we have to keep the infers here or TS3.4 will break
+        : T[K] extends IAnyType
         ? T[K]
         : never
 }
