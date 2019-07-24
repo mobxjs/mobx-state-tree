cd8537de4ea09e8b2f040056774e150ebb6be329
kga <kga@skbkontur.ru>
2019-07-24 20:45:31 +0500

    add tests

---
+++ packages/mobx-state-tree/__tests__/core/model.test.ts
@@ -0,0 +1,55 @@
+import { getSnapshot, types } from "../../src"
+
+test("it should allow valid names", () => {
+    let didThrow = false
+    try {
+        types.model("My_Model")
+        types.model("MyModel@82d92470-d05e-4dab-92cc-25b45f7fa2e4")
+        types.model("MyModel$$$")
+    } catch (e) {
+        didThrow = true
+    }
+    expect(didThrow).toBe(false)
+})
+
+describe("it should throw on invalid names", () => {
+    test("begins with underscore", () => {
+        let didThrow = false
+        try {
+            types.model("_MyModel")
+        } catch (e) {
+            didThrow = true
+        }
+        expect(didThrow).toBe(true)
+    })
+
+    test("begins with dash", () => {
+        let didThrow = false
+        try {
+            types.model("-MyModel")
+        } catch (e) {
+            didThrow = true
+        }
+        expect(didThrow).toBe(true)
+    })
+
+    test("begins with at", () => {
+        let didThrow = false
+        try {
+            types.model("@MyModel")
+        } catch (e) {
+            didThrow = true
+        }
+        expect(didThrow).toBe(true)
+    })
+
+    test("begins with dollar sign", () => {
+        let didThrow = false
+        try {
+            types.model("$MyModel")
+        } catch (e) {
+            didThrow = true
+        }
+        expect(didThrow).toBe(true)
+    })
+})
