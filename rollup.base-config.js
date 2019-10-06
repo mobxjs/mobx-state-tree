2ec5c65804286b377b56192ae6c8fb2f1b5423ef
Leon van Ginneken <leon.van.ginneken@alliander.com>
2019-10-06 16:36:28 +0200

    Make umd debug build work without 'process' global (#1351)
    * Make devMode() work without process global
    
    * bind development environment to development build
    
    * set the NODE_ENV only for debug umd-builds

--- rollup.base-config.js
+++ rollup.base-config.js
@@ -6,6 +6,13 @@ import replace from "rollup-plugin-replace"
 
 const devPlugins = () => [resolve(), filesize()]
 
+// For umd builds, set process.env.NODE_ENV to "development" since 'process' is not available in the browser
+const devPluginsUmd = () => [
+    resolve(),
+    replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+    filesize()
+]
+
 const prodPlugins = () => [
     resolve(),
     replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
@@ -22,5 +29,6 @@ export const baseConfig = ({ input, globals, umdName, external, outFile, format,
         name: format === "umd" ? umdName : undefined
     },
     external,
-    plugins: mode === "production" ? prodPlugins() : devPlugins()
+    plugins:
+        mode === "production" ? prodPlugins() : format === "umd" ? devPluginsUmd() : devPlugins()
 })
