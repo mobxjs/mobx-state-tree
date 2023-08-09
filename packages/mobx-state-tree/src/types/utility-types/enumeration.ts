18d2e5071ee01b13011684fd996b01790fca71d1
Jamon Holmgren <jamonholmgren@gmail.com>
2023-08-09 15:29:25 -0700

    fix type a bit

--- packages/mobx-state-tree/src/types/utility-types/enumeration.ts
+++ packages/mobx-state-tree/src/types/utility-types/enumeration.ts
@@ -3,18 +3,18 @@ import { ISimpleType, union, literal, assertIsString, devMode } from "../../inte
 /** @hidden */
 export type UnionStringArray<T extends readonly string[]> = T[number]
 
-// strongly typed enumeration forms for plain string arrays (when passed directly to the function)
-// with these overloads we also get correct typing for native TS string enums when we use Object.values(Enum) as Enum[] as options
-// also these overloads make types.enumeration<Enum>(Object.values(Enum)) possible
-// the only case where this doesn't work is when passing to the function an arrays variable, for these cases
-// it will just fallback and assume the type is a generic string
+// strongly typed enumeration forms for plain and readonly string arrays (when passed directly to the function).
+// with these overloads, we get correct typing for native TS string enums when we use Object.values(Enum) as Enum[] as options.
+// these overloads also allow both mutable and immutable arrays, making types.enumeration<Enum>(Object.values(Enum)) possible.
+// the only case where this doesn't work is when passing to the function an array variable with a mutable type constraint;
+// for these cases, it will just fallback and assume the type is a generic string.
 export function enumeration<T extends readonly string[]>(
     options: T
 ): ISimpleType<UnionStringArray<T>>
-export function enumeration<T extends readonly string[]>(
+export function enumeration<T extends string>(
     name: string,
-    options: T
-): ISimpleType<UnionStringArray<T>>
+    options: T[]
+): ISimpleType<UnionStringArray<T[]>>
 
 /**
  * `types.enumeration` - Can be used to create an string based enumeration.
