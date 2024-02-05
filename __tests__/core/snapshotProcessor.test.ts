6e3440c340fe56728e2f561443ddfd1c6e7219f3
Kim <kimgronqvist@gmail.com>
2024-02-05 11:12:43 +0100

    Add test case for incorrect processor

--- __tests__/core/snapshotProcessor.test.ts
+++ __tests__/core/snapshotProcessor.test.ts
@@ -843,4 +843,14 @@ describe("snapshotProcessor", () => {
     expect(() => store.setProp("b")).not.toThrow()
     expect(store.prop).toBe("b")
   })
+
+  test("it should fail if given incorrect processor", () => {
+    expect(() => {
+      types.model({
+        m: types.snapshotProcessor(types.number, {
+          postProcessor: {} as any
+        })
+      })
+    }).toThrowError("[mobx-state-tree] postSnapshotProcessor must be a function")
+  })
 })
