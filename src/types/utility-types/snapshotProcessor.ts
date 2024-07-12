346360db1f77ed657a628fc1eaa725a06c33ea2e
Tyler Williams <tyler@coolsoftware.dev>
2024-07-11 19:52:29 -0600

    docs: bump JSdocon snapshotProcessor is

--- src/types/utility-types/snapshotProcessor.ts
+++ src/types/utility-types/snapshotProcessor.ts
@@ -149,6 +149,21 @@ class SnapshotProcessor<IT extends IAnyType, CustomC, CustomS> extends BaseType<
     return this._subtype
   }
 
+  /**
+   * MST considers a given value to "be" of a subtype is the value is either:
+   *
+   * 1. And instance of the subtype
+   * 2. A valid snapshot *in* of the subtype
+   *
+   * Before v7, we used to also consider processed models (as in, SnapshotOut values of this).
+   * This is no longer the case, and is more in line with our overall "is" philosophy, which you can
+   * see in `src/core/type/type.ts:104` (assuming lines don't change too much).
+   *
+   * The `is` function specifically checks for `SnapshotIn` or `Instance` of a given type.
+   *
+   * @param thing
+   * @returns
+   */
   is(thing: any): thing is any {
     const value = isType(thing)
       ? this._subtype
