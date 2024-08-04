217cfe385d3512eb161e949ed53dc39f797d3cc3
Jason Gedge <jason@gadget.dev>
2024-08-04 17:31:02 -0400

    Simplify dev/prod testing scripts

--- scripts/bun-test-dev.sh
+++ scripts/bun-test-dev.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-# Run the command in development mode and store the outputs in a temporary file
-temp_file_dev=$(mktemp)
-NODE_ENV=development find . -wholename './__tests__/*test*' -exec bun test {} \; > "$temp_file_dev" 2>&1
-
-# Check for failures in the outputs
-if grep -q "(fail)" "$temp_file_dev"; then
-    echo "Development tests failed."
-    cat "$temp_file_dev"
-    exit 1
-else
-    echo "All tests passed."
-    exit 0
-fi
+export NODE_ENV=development
+export MST_TESTING=1
+find . -wholename './__tests__/*.test.ts' -print0 | xargs -0 bun test
